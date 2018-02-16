993026dacfeb232e7ca334aa71f80334da5af8c9
真田雄太 <>
2018-02-16 19:37:26 +0900

    未着手obcファイル

--- MySmartNews/NewsViewController.h
+++ MySmartNews/NewsViewController.h
@@ -10,4 +10,6 @@
 
 @interface NewsViewController : UIViewController
 
+@property(nonatomic)NSString *urlString;
+
 @end
--- MySmartNews/NewsViewController.m
+++ MySmartNews/NewsViewController.m
@@ -7,17 +7,96 @@
 //
 
 #import "NewsViewController.h"
+#import <MRProgress/MRProgress.h>
 
-@interface NewsViewController ()
+@interface NewsViewController ()<UIWebViewDelegate>
 
 @end
 
 @implementation NewsViewController
+{
+    
+    IBOutlet UIWebView *webView;
+    
+    MRProgressOverlayView *progressView;
+}
+
+@synthesize urlString;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    webView.delegate = self;
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [webView loadRequest:request];
+    
+    
+    
+    
     // Do any additional setup after loading the view.
 }
+
+
+
+- (IBAction)back:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+
+//WebViewが読み込みを開始した時に勝手に呼ばれるデリゲートメソッド
+-(void)webViewDidStartLoad:(UIWebView *)webView{
+
+    if(!progressView){
+        progressView = [MRProgressOverlayView new];
+        progressView.mode = MRProgressOverlayViewModeIndeterminate;
+        [self.view addSubview:progressView];
+        [progressView setTitleLabelText:@"よみこみ中"];
+        [progressView show:YES];
+        
+    }
+
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView{
+
+//        [MRProgressOverlayView dismissOverlayForView:self.view animated:YES];
+
+    [MRProgressOverlayView dismissOverlayForView:self.view animated:YES completion:nil];
+    
+    
+}
+
+
+
+/*
+-(void)webViewDidStartLoad:(UIWebView*)webView{
+    //    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    if(!progressView){
+        progressView = [MRProgressOverlayView new];
+        progressView.mode = MRProgressOverlayViewModeIndeterminate;
+        [self.view addSubview:progressView];
+        [progressView setTitleLabelText:@"Loading ..."];
+        [progressView show:YES];
+    }
+    
+}
+
+// ページ読込完了時にインジケータを非表示にする
+-(void)webViewDidFinishLoad:(UIWebView*)webView{
+    //    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [MRProgressOverlayView dismissOverlayForView:self.view animated:YES];
+}*/
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
