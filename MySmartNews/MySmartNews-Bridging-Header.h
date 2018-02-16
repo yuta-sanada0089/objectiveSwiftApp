19268895fbd972421c2b612d32c8407f22d4ea76
真田雄太 <>
2018-02-16 19:39:04 +0900

    練習用ソースコード

--- MySmartNews/MySmartNews-Bridging-Header.h
+++ MySmartNews/MySmartNews-Bridging-Header.h
@@ -2,3 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "MSSTabbedPageViewController.h"
+#import "DDXML.h"
+#import "NewsViewController.h"
+
